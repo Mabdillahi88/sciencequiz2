8d11c62396972349cb3b1148bbb779c0b3bd485e
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2022-12-04 14:15:57 +0000

    added basic js template to js pages

--- assets/js/scramble.js
+++ assets/js/scramble.js
@@ -1,1 +1,6 @@
-
+const wordText = document.querySelector(".keywords"),
+hintText = document.querySelector(".help-hint span"),
+timeText = document.querySelector(".time-remaining b"),
+inputField = document.querySelector("input"),
+refreshBtn = document.querySelector(".new-keyword"),
+checkBtn = document.querySelector(".correct-science-word");
