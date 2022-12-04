80e81a0e3175dbb42238985ff16d16aa09f18e62
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2022-12-04 14:41:54 +0000

    completed 3 questions for js script

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,11 @@ let words = [
         word: "Cells",
         hint: "The building blocks of life"
     },
+    {
+        word: "Circuits",
+        hint: "How electricity flows around"
+    },
+    {
+        word: "Digestion",
+        hint: "How food is broken down"
+    },
