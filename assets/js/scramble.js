1fe0b2087e4385df286ca1fc4d3855f6d3d82400
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2022-12-04 14:46:22 +0000

    added correct word and timer js to js script

--- assets/js/scramble.js
+++ assets/js/scramble.js
@@ -4,3 +4,16 @@ timeText = document.querySelector(".time-remaining b"),
 inputField = document.querySelector("input"),
 refreshBtn = document.querySelector(".new-keyword"),
 checkBtn = document.querySelector(".correct-science-word");
+
+let correctWord, timer;
+const initTimer = maxTime => {
+    clearInterval(timer);
+    timer = setInterval(() => {
+        if(maxTime > 0) {
+            maxTime--;
+            return timeText.innerText = maxTime;
+        }
+        alert(`Time off! ${correctWord.toUpperCase()} was the correct word`);
+        initGame();
+    }, 1000);
+}
