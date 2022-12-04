d1ad0bc5bd56386f6f4b3f071c8784e9608c0c38
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2022-12-04 15:18:07 +0000

    added the js script for random scramble script to js page

--- assets/js/scramble.js
+++ assets/js/scramble.js
@@ -17,3 +17,19 @@ const initTimer = maxTime => {
         initGame();
     }, 1000);
 }
+
+const initGame = () => {
+    initTimer(10);
+    let randomObj = words[Math.floor(Math.random() * words.length)];
+    let wordArray = randomObj.word.split("");
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+    }
+    wordText.innerText = wordArray.join("");
+    hintText.innerText = randomObj.hint;
+    correctWord = randomObj.word.toLowerCase();;
+    inputField.value = "";
+    inputField.setAttribute("maxlength", correctWord.length);
+}
+initGame();
