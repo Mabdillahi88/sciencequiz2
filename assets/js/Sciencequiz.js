d14c05c68952cce562a743dd42bae6be2528cda0
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2022-12-06 08:04:01 +0000

    add 5 questions to js script page

--- assets/js/Sciencequiz.js
+++ assets/js/Sciencequiz.js
@@ -10,4 +10,48 @@ let questions = [
       "All of the above"
     ]
   },
-  
+  {
+    numb: 2,
+    question: "What is a limiting factor",
+    answer: "A factor which, if in short supply limits or reduces the rate of photosynthesis",
+    options: [
+      "A factor which, if in short supply limits or reduces the rate of photosynthesis",
+      "An enzyme which increases the rate of the reaction",
+      "An inhibitor of an enzyme which decreases the rate of the reaction",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Photosynthesis is what kind of reaction?",
+    answer: "Endothermic",
+    options: [
+      "Exothermic",
+      "Endothermic",
+      "Phototropic",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of these is a limiting factor of photosynthesis?,
+    answer: "Carbon dioxide",
+    options: [
+      "Oxygen",
+      "Carbon dioxide",
+      "Methane",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of these will increase the rate of photosynthesis?",
+    answer: "Light intensity increases",
+    options: [
+      "Light intensity increases",
+      "Light intensity decreases",
+      "eLight intensity remains constant",
+      "All of the above"
+    ]
+  },
+];
