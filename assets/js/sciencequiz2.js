7d756fdaf3a3f7ba49dfac48ef10e4dfdc2d6cef
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2022-12-06 10:40:57 +0000

    added begin and leave click js script to quiz page

--- assets/js/sciencequiz2.js
+++ assets/js/sciencequiz2.js
@@ -1,10 +1,19 @@
-const start_btn = document.querySelector(".begin_btn button");
-const info_box = document.querySelector(".instructions");
-const exit_btn = info_box.querySelector(".buttons .leave");
-const continue_btn = info_box.querySelector(".buttons .continue");
+const begin_btn  = document.querySelector(".begin_btn button");
+const instructions = document.querySelector(".instructions");
+const leave = instructions.querySelector(".buttons .leave");
+const continue_btn = instructions.querySelector(".buttons .continue");
 const quiz_box = document.querySelector(".questions");
 const result_box = document.querySelector(".final_score");
 const option_list = document.querySelector(".rules_game");
 const time_line = document.querySelector("header .straight-line");
 const timeText = document.querySelector(".timer .how-long");
 const timeCount = document.querySelector(".how-long-time");
+
+begin_btn .onclick = ()=>{
+    instructions.classList.add("activeInfo"); 
+}
+
+
+leave.onclick = ()=>{
+    instructions.classList.remove("activeInfo"); 
+}
