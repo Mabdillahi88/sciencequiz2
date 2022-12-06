d6dc5e94936f3ba1c0b8f69d7a17ad0ade01939f
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2022-12-06 10:32:33 +0000

    added const declaration to js fucntion script

--- assets/js/sciencequiz2.js
+++ assets/js/sciencequiz2.js
@@ -1,2 +1,10 @@
 const start_btn = document.querySelector(".begin_btn button");
 const info_box = document.querySelector(".instructions");
+const exit_btn = info_box.querySelector(".buttons .leave");
+const continue_btn = info_box.querySelector(".buttons .continue");
+const quiz_box = document.querySelector(".questions");
+const result_box = document.querySelector(".final_score");
+const option_list = document.querySelector(".rules_game");
+const time_line = document.querySelector("header .straight-line");
+const timeText = document.querySelector(".timer .how-long");
+const timeCount = document.querySelector(".how-long-time");
