8bbdc66317cdccd7bd74dbbd116fceb4b70d9f7d
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2022-12-09 13:01:16 +0000

    new questions with hints add to js script page

---
+++ assets/js/questions_quiz.js
@@ -0,0 +1,34 @@
+const wordList = [
+  {
+      word: "respiration",
+      hint: "how energy is made inside living cells"
+  },
+  {
+      word: "photosynthesis",
+      hint: "the chemcical reaction that takes place inside plant cells"
+  },
+  {
+      word: "chlorophyll",
+      hint: "the pigment that makes plant green"
+  },
+  {
+      word: "digestion",
+      hint: "how food is broken down inside our body after we eat food"
+  },
+  {
+      word: "electricity",
+      hint: "the power source that fuels light bulbs"
+  },
+  {
+      word: "reproduction",
+      hint: "how offspring are made"
+  },
+  {
+      word: "mitosis",
+      hint: "when cells make identical copy"
+  },
+  {
+      word: "fertilisation",
+      hint: "when sperm and egg cell fused together"
+  },
+]
