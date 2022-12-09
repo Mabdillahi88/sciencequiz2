9d8459811dcb1c307faa810ba236abad6bd02733
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2022-12-09 13:02:39 +0000

    add basic const tags for js script page

---
+++ assets/js/quiz.js
@@ -0,0 +1,7 @@
+const facts = document.querySelector(".facts"),
+assistance = document.querySelector(".assistance span"),
+chances = document.querySelector(".chances span"),
+incorrect = document.querySelector(".incorrect span"),
+leave = document.querySelector(".leave"),
+importantInformation = document.querySelector(".key-information");
+
