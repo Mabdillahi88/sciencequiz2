89f1f40950cd3529f344a484a3e1967fe6f05d0d
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2022-12-09 13:06:33 +0000

    updated randomword function

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -22,3 +22,37 @@ function randomWord() {
         facts.innerHTML = html;
     }
 }
+randomWord();
+
+function initGame(e) {
+    let key = e.target.value.toLowerCase();
+    if(key.match(/^[A-Za-z]+$/) && !incorrectLetters.includes(` ${key}`) && !correctLetters.includes(key)) {
+        if(word.includes(key)) {
+            for (let i = 0; i < word.length; i++) {
+                if(word[i] == key) {
+                    correctLetters += key;
+                    facts.querySelectorAll("input")[i].value = key;
+                }
+            }
+        } else {
+            maxGuesses--;
+            incorrectLetters.push(` ${key}`);
+        }
+        chances.innerText = maxGuesses;
+        incorrect.innerText = incorrectLetters;
+    }
+    importantInformation.value = "";
+
+    setTimeout(() => {
+        if(correctLetters.length === word.length) {
+            alert(`Congrats! You found the word ${word.toUpperCase()}`);
+            return randomWord();
+        } else if(maxGuesses < 1) {
+            alert("Game over! You don't have remaining guesses");
+            for(let i = 0; i < word.length; i++) {
+                facts.querySelectorAll("input")[i].value = word[i];
+            }
+        }
+    }, 100);
+}
+
