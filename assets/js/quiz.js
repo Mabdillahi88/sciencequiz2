305d3f72376155f2d9d413d959525119b462344b
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2022-12-09 13:04:44 +0000

    add function js tags to make words random

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -5,3 +5,20 @@ incorrect = document.querySelector(".incorrect span"),
 leave = document.querySelector(".leave"),
 importantInformation = document.querySelector(".key-information");
 
+let word, maxGuesses, incorrectLetters = [], correctLetters = [];
+
+function randomWord() {
+    let ranItem = wordList[Math.floor(Math.random() * wordList.length)];
+    word = ranItem.word;
+    maxGuesses = word.length >= 5 ? 8 : 6;
+    correctLetters = []; incorrectLetters = [];
+    assistance.innerText = ranItem.hint;
+    chances.innerText = maxGuesses;
+    incorrect.innerText = incorrectLetters;
+
+    let html = "";
+    for (let i = 0; i < word.length; i++) {
+        html += `<input type="text" disabled>`;
+        facts.innerHTML = html;
+    }
+}
